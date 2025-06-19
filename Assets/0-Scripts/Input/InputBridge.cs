f1969c8c1f776ce12aa028ef48db11f39f83e54d
classworm <classworm@icloud.com>
2025-06-19 15:41:52 +0300

    Implemented movement, anim blending and more
    Added free prototype assets for environment and character
    Added basic movement animations
    Implemented blend tree for strafe mode

--- Assets/0-Scripts/Input/InputBridge.cs
+++ Assets/0-Scripts/Input/InputBridge.cs
@@ -5,17 +5,13 @@ using UnityEngine.InputSystem;
 public class InputBridge : MonoBehaviour
 {
     public Vector2 MoveInput => _moveInput;
-    public Vector2 LookInput => _lookInput;
 
     private Vector2 _moveInput;
-    private Vector2 _lookInput;
     private bool _canMove;
-    private bool _canLook;
 
     private void Awake()
     {
         EnableMovement(true);
-        EnableLooking(true);
     }
 
     private void Start()
@@ -24,18 +20,11 @@ public class InputBridge : MonoBehaviour
     }
 
     public void EnableMovement(bool enable) => _canMove = enable;
-    public void EnableLooking(bool enable) => _canLook = enable;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         if (!_canMove) return;
         _moveInput = context.ReadValue<Vector2>();
-    }
-
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        if (!_canLook) return;
-        _lookInput = context.ReadValue<Vector2>();
     }
 
     private void SetCursorState(bool value)
--- Assets/0-Scripts/Player/ThirdPersonController.cs
+++ Assets/0-Scripts/Player/ThirdPersonController.cs
@@ -1,31 +1,42 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController), typeof(InputBridge), typeof(Animator))]
 public class ThirdPersonController : MonoBehaviour
 {
-    [Header("Camera Settings")]
-    [SerializeField] private Transform _cameraTransform;
+    [Header("References")]
+    [Tooltip("Main Camera that has a CinemachineBrain on it")]
+    [SerializeField] private Camera _cinemachineCamera;
 
     [Header("Movement Settings")]
     [SerializeField] private float _walkSpeed = 5f;
     [SerializeField] private float _turnSmoothTime = 0.1f;
     [SerializeField] private float _gravity = -9.81f;
 
+    [Header("Animation Settings")]
+    [Tooltip("Damp time for blending animation parameters")]
+    [SerializeField] private float _animDampTime = 0.1f;
+
     private InputBridge _inputBridge;
     private CharacterController _controller;
+    private Animator _animator;
     private Vector3 _velocity;
     private float _turnSmoothVelocity;
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
-        if (_cameraTransform == null && Camera.main != null)
-            _cameraTransform = Camera.main.transform;
+        _inputBridge = GetComponent<InputBridge>();
+        _animator = GetComponent<Animator>();
+
+        if (_cinemachineCamera == null && Camera.main != null)
+            _cinemachineCamera = Camera.main;
     }
 
     private void Update()
     {
         ApplyGravity();
-        MoveAndRotate();
+        Move();
+        UpdateAnimator();
     }
 
     private void ApplyGravity()
@@ -37,24 +48,57 @@ public class ThirdPersonController : MonoBehaviour
         _controller.Move(_velocity * Time.deltaTime);
     }
 
-    private void MoveAndRotate()
+    private void Move()
     {
-        Vector3 inputDir = new Vector3(_inputBridge.MoveInput.x, 0f, _inputBridge.MoveInput.y).normalized;
-        if (inputDir.magnitude < 0.1f)
+        Vector2 mi = _inputBridge != null
+                     ? _inputBridge.MoveInput
+                     : Vector2.zero;
+
+        Vector3 inputDir = new Vector3(mi.x, 0f, mi.y).normalized;
+        if (inputDir.sqrMagnitude < 0.01f)
             return;
 
-        float targetAngle = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg
-                            + _cameraTransform.eulerAngles.y;
+        float inputAngle = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg;
+
+        float camYaw = _cinemachineCamera != null
+                       ? _cinemachineCamera.transform.eulerAngles.y
+                       : 0f;
+
+        float targetAngle = inputAngle + camYaw;
+
         float smoothedAngle = Mathf.SmoothDampAngle(
             transform.eulerAngles.y,
             targetAngle,
             ref _turnSmoothVelocity,
             _turnSmoothTime
         );
-
         transform.rotation = Quaternion.Euler(0f, smoothedAngle, 0f);
 
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-        _controller.Move(moveDir.normalized * _walkSpeed * Time.deltaTime);
+        _controller.Move(moveDir * _walkSpeed * Time.deltaTime);
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector2 mi = _inputBridge.MoveInput;
+        Vector3 camF = _cinemachineCamera.transform.forward;
+        camF.y = 0; camF.Normalize();
+        Vector3 camR = _cinemachineCamera.transform.right;
+        camR.y = 0; camR.Normalize();
+
+        Vector3 worldMove = camR * mi.x + camF * mi.y;
+        float speed = Mathf.Clamp01(worldMove.magnitude);
+
+        float h = 0f, v = 0f;
+        if (speed > 0.01f)
+        {
+            Vector3 localDir = transform.InverseTransformDirection(worldMove.normalized);
+            h = localDir.x;
+            v = localDir.z;
+        }
+
+        _animator.SetFloat("Horizontal", h, _animDampTime, Time.deltaTime);
+        _animator.SetFloat("Vertical", v, _animDampTime, Time.deltaTime);
+        _animator.SetFloat("Speed", speed);
     }
 }
